8ed60423640e20ea1509d7c77a9d9280a11a233c
Harsh Singh Sikarwar <harshsikarwar2005@gmail.com>
2026-06-21 13:05:27 +0530

    deploy:

--- mobile_store/settings.py
+++ mobile_store/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b91g*r(26&5sb8fjacxed!11p4gnv)5mkb91o8&pl09(a=5kq)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    ".onrender.com",
+]
 
 
 # Application definition
